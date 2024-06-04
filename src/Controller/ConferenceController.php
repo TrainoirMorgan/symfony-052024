b8184a2d71aadc8ef28ea796082c4498505d6798
Morgan Trainoir <morgantrainoir.pro@gmail.com>
2024-06-04 13:55:25 +0000

    tests

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,6 +8,7 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,6 +37,7 @@ class ConferenceController extends AbstractController
   public function show(
     Request $request,
     Conference $conference,
+    SpamChecker $spamChecker,
     CommentRepository $commentRepository,
     #[Autowire('%photo_dir%')] string $photoDir,
   ): Response {
@@ -57,6 +59,17 @@ class ConferenceController extends AbstractController
       }
 
       $this->entityManager->persist($comment);
+
+      $context = [
+        'user_ip' => $request->getClientIp(),
+        'user_agent' => $request->headers->get('user-agent'),
+        'referrer' => $request->headers->get('referer'),
+        'permalink' => $request->getUri(),
+      ];
+      if (2 === $spamChecker->getSpamScore($comment, $context)) {
+        throw new \RuntimeException('Blatant spam, go away!');
+      }
+
       $this->entityManager->flush();
 
       return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
